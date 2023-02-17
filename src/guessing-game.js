eb1283a5fe9e2d97eb47b984f897f870647a58f4
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2023-02-17 17:59:09 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        const minimum = this.min
+        const maximum = this.max
+        return Math.round((minimum + maximum) / 2)
     }
 
     lower() {
-
+        this.max = this.guess()
     }
 
     greater() {
-
+        this.min = this.guess()
     }
 }
+
 
 module.exports = GuessingGame;
